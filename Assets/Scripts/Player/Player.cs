148b94f2df551f4bbf77bc694eb7bf9e07ea8432
Eddy <eddy.abada.ea@gmail.com>
2021-01-31 17:43:57 +0100

    Player cover hands anim

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public Transform playerEyes;
+
+    float detection_distance = 3.5f;
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(playerEyes.position, playerEyes.forward, out hit, detection_distance))
+        {
+            //hit objects
+            //get type of interactible (Doors, Lamps, Frames and Paintings)
+            if (hit.transform.gameObject.GetComponent<Interactible>())
+            {
+                Debug.Log("Interactible");
+            }
+            Debug.DrawRay(playerEyes.position, playerEyes.forward * hit.distance, Color.yellow);
+        }
+    }
 }

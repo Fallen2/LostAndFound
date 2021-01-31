e5daa539f075d6409c50cc23ade4367c956c967e
Eddy <eddy.abada.ea@gmail.com>
2021-01-31 15:23:10 +0100

    Character Controller

---
+++ Assets/Scripts/Interactible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    public void OnTrigger()
+    {
+
+    }
+
+    public void OnUse()
+    {
+
+    }
+}
